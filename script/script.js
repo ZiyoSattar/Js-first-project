589201342a246d21114f9f6e80c6a0df01b0d1c2
ZiyoSattar <muhhammadziyos@gmail.com>
2022-07-28 19:56:37 +0500

    home work

---
+++ script/script.js
@@ -0,0 +1,23 @@
+console.log(alert("Ariza Topshirishga tayyormisiz ?"));
+
+console.log(prompt("Ismingiz va Familiya ?"));
+
+console.log(prompt("Yoshingiz ?"));
+
+console.log(prompt("Maxalla Turar joyingiz ?"));
+
+console.log(prompt("Oilada nechta farzand ?"));
+
+console.log(prompt("Tug'ilgan kun, oy va yilingiz ?"));
+
+console.log(prompt("Nimalarga qiziqasiz ?"));
+
+console.log(prompt("Qaysi oquv yurtida oqiysiz ?"));
+
+console.log(prompt("Nechinchi sinif / kurs  ?"));
+
+console.log(prompt("Nechta horijiy tilni bilasiz ?"));
+
+console.log(prompt("Dasturchilar haqida fikringiz ?"));
+
+console.log(alert("Ma'lumotlaringiz uchun rahmat !)"));
